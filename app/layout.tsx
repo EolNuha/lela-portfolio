dedeb429d48086ae4be878180034e33c46484c32
Eol Nuha <eol.nuha22@gmail.com>
2026-09-19 23:09:20 +0200

    top status white

--- app/layout.tsx
+++ app/layout.tsx
@@ -26,11 +26,8 @@ export const metadata: Metadata = {
 }
 
 export const viewport: Viewport = {
-  colorScheme: 'light dark',
-  themeColor: [
-    { media: '(prefers-color-scheme: light)', color: '#171512' },
-    { media: '(prefers-color-scheme: dark)', color: '#171512' },
-  ],
+  colorScheme: 'light',
+  themeColor: '#ffffff',
   viewportFit: 'cover',
 }
 
